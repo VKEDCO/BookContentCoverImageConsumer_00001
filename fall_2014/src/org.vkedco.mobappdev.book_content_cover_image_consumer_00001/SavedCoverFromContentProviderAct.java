48b9aae7cb93e23a69612ffc932e1f80e3b6ada8
vkedco <vladimir.kulyukin@gmail.com>
2014-12-03 10:48:39 -0700

    Update SavedCoverFromContentProviderAct.java

--- fall_2014/src/org.vkedco.mobappdev.book_content_cover_image_consumer_00001/SavedCoverFromContentProviderAct.java
+++ fall_2014/src/org.vkedco.mobappdev.book_content_cover_image_consumer_00001/SavedCoverFromContentProviderAct.java
@@ -1,3 +1,5 @@
+package org.vkedco.mobappdev.book_content_cover_image_consumer_00001;
+
 /**
  ****************************************************
  * Bugs to vladimir dot kulyukin at gmail dot com
